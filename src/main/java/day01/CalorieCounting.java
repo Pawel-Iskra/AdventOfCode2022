78df7ff190555e93f244894fe093b0f56dff7ce9
Pawel <paweliszcz@wp.pl>
2022-12-02 08:26:37 +0100

    [AoC2022] day01

---
+++ src/main/java/day01/CalorieCounting.java
@@ -0,0 +1,16 @@
+package day01;
+
+import utils.MyUtilities;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CalorieCounting {
+
+    public static void main(String[] args) throws IOException {
+        String pathToInputFile = "src/main/resources/day01/input.txt";
+        List<String> inputLines = MyUtilities.getInputLines(pathToInputFile);
+
+
+    }
+}

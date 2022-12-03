f82d39d8fbd70542f1157bdd97aa76acdd1c1d18
Pawel <paweliszcz@wp.pl>
2022-12-03 08:14:38 +0100

    [AoC2022] day03

--- src/main/java/day03/RucksackReorganization.java
+++ src/main/java/day03/RucksackReorganization.java
@@ -7,67 +7,53 @@ import java.util.List;
 
 public class RucksackReorganization {
 
-    private static long getResultForPartTwo(List<String> inputLines){
-        System.out.println("PART TWO:");
+    private static long getResultForPartTwoRules(List<String> inputLines) {
         long result = 0;
         int size = inputLines.size();
-        for (int i = 0; i < size; i= i+3) {
-            String first = inputLines.get(i);
-            String second = inputLines.get(i + 1);
-            String third = inputLines.get(i + 2);
+        for (int i = 0; i < size; i = i + 3) {
+            String firstRucksack = inputLines.get(i);
+            String secondRucksack = inputLines.get(i + 1);
+            String thirdRucksack = inputLines.get(i + 2);
 
-            int value = 0;
-            for (int j = 0; j < first.length(); j++) {
-                char currentChar = first.charAt(j);
+            int itemPriority = 0;
+            for (int j = 0; j < firstRucksack.length(); j++) {
+                char currentChar = firstRucksack.charAt(j);
                 String currentCharStr = String.valueOf(currentChar);
-                if(second.contains(currentCharStr) && third.contains(currentCharStr)){
-                    System.out.println("currentCharStr = " + currentCharStr);
-                    if(currentChar >= 'A' && currentChar <= 'Z'){
-                        value = currentChar - 38;
+                if (secondRucksack.contains(currentCharStr) && thirdRucksack.contains(currentCharStr)) {
+                    if (currentChar >= 'A' && currentChar <= 'Z') {
+                        itemPriority = currentChar - 38;
                     } else {
-                        value = currentChar - 96;
+                        itemPriority = currentChar - 96;
                     }
-                    result += value;
                     break;
                 }
             }
+            result += itemPriority;
         }
-
         return result;
     }
 
-    private static long getResult(List<String> inputLines) {
+    private static long getResultForPartOneRules(List<String> inputLines) {
         long result = 0;
-
-        int size = inputLines.size();
-        for (int i = 0; i < size; i++) {
-            String rucksack = inputLines.get(i);
+        for (String rucksack : inputLines) {
             int length = rucksack.length();
             String firstCompartment = rucksack.substring(0, length / 2);
             String secondCompartment = rucksack.substring(length / 2);
 
-//            System.out.println("firstCompartment = " + firstCompartment);
-//            System.out.println("secondCompartment = " + secondCompartment);
-
-            int sumOfRucksack = 0;
-            for (int j = 0; j < firstCompartment.length(); j++) {
-                char currentChar = firstCompartment.charAt(j);
-                int value = 0;
-                if(secondCompartment.contains(String.valueOf(currentChar))){
-//                    System.out.println("currentChar = " + currentChar);
-                    if(currentChar >= 'A' && currentChar <= 'Z'){
-                        value = currentChar - 38;
+            int itemPriority = 0;
+            for (int i = 0; i < firstCompartment.length(); i++) {
+                char currentChar = firstCompartment.charAt(i);
+                if (secondCompartment.contains(String.valueOf(currentChar))) {
+                    if (currentChar >= 'A' && currentChar <= 'Z') {
+                        itemPriority = currentChar - 38;
                     } else {
-                        value = currentChar - 96;
+                        itemPriority = currentChar - 96;
                     }
-                    sumOfRucksack= value;
+                    break;
                 }
             }
-            System.out.println("sumOfRucksack = " + sumOfRucksack);
-
-            result +=sumOfRucksack;
-        } // end inputlinesa
-
+            result += itemPriority;
+        }
         return result;
     }
 
@@ -75,19 +61,8 @@ public class RucksackReorganization {
     public static void main(String[] args) throws IOException {
         String pathToInputFile = "src/main/resources/day03/input.txt";
         List<String> inputLines = MyUtilities.getInputLines(pathToInputFile);
-        int size = inputLines.size();
-        for (int i = 0; i < size; i++) {
-            String line = inputLines.get(i);
-            System.out.println(i + ". " + line);
-        }
-        System.out.println();
 
-        long resultt = getResult(inputLines);
-        System.out.println("resultt = " + resultt);
-
-        long resultPartTwo = getResultForPartTwo(inputLines);
-        System.out.println("resultPartTwo = " + resultPartTwo);
-
-
+        System.out.println("Part 1 = " + getResultForPartOneRules(inputLines));
+        System.out.println("Part 2 = " + getResultForPartTwoRules(inputLines));
     }
 }

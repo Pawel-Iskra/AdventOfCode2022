00cdbd3cf2738fac2f664183b44aa4bab874839f
Pawel <paweliszcz@wp.pl>
2022-12-05 15:27:12 +0100

    [AoC2022] day04 finished

--- src/main/java/day04/CampCleanup.java
+++ src/main/java/day04/CampCleanup.java
@@ -5,14 +5,14 @@ import utils.MyUtilities;
 import java.io.IOException;
 import java.util.List;
 
-public class Day04 {
+public class CampCleanup {
 
-    private static long getResultForPartTwoRules(List<String> inputLines) {
-        long result = 0;
-        int size = inputLines.size();
+    private static void goOverBigListOfTheSectionAssignmentsForElfs(List<String> listOfSectionAssignmentsForElfs) {
+        long counterOfPairsThatOneSectionIsWithinSecondOne = 0;
+        long counterOfPairsThatOverlapsAtAll = 0;
+        int size = listOfSectionAssignmentsForElfs.size();
         for (int i = 0; i < size; i++) {
-            String line = inputLines.get(i);
-            System.out.println(i + ". line = " + line);
+            String line = listOfSectionAssignmentsForElfs.get(i);
             String[] pair = line.split(",");
             String[] first = pair[0].split("-");
             String[] second = pair[1].split("-");
@@ -22,54 +22,23 @@ public class Day04 {
             int secondStart = Integer.parseInt(second[0]);
             int secondEnd = Integer.parseInt(second[1]);
 
-            if((firstStart <= secondStart && firstEnd >= secondStart) ||
-                    (secondStart <= firstStart && secondEnd >= firstStart)){
-                System.out.println("HERE");
-                result++;
-            } else {
-                System.out.println("HERE NOT");
-            }
-        }
-        return result;
-    }
-
-    private static long getResultForPartOneRules(List<String> inputLines) {
-        long result = 0;
-        int size = inputLines.size();
-        for (int i = 0; i < size; i++) {
-            String line = inputLines.get(i);
-           // System.out.println(i + ". line = " + line);
-            String[] pair = line.split(",");
-            String[] firstPair = pair[0].split("-");
-            String[] secondPair = pair[1].split("-");
-
-            int firstStart = Integer.parseInt(firstPair[0]);
-            int firstEnd = Integer.parseInt(firstPair[1]);
-            int secondStart = Integer.parseInt(secondPair[0]);
-            int secondEnd = Integer.parseInt(secondPair[1]);
-
-//            System.out.println("firstStart = " + firstStart);
-//            System.out.println("firstEnd = " + firstEnd);
-//            System.out.println("secondStart = " + secondStart);
-//            System.out.println("secondEnd = " + secondEnd);
-
-            if((firstStart <= secondStart && firstEnd >= secondEnd) ||
-                    (firstStart >= secondStart && firstEnd <= secondEnd)){
-                result++;
+            if ((firstStart <= secondStart && firstEnd >= secondEnd) ||
+                    (firstStart >= secondStart && firstEnd <= secondEnd)) {
+                counterOfPairsThatOneSectionIsWithinSecondOne++;
             }
 
+            if ((firstStart <= secondStart && firstEnd >= secondStart) ||
+                    (secondStart <= firstStart && secondEnd >= firstStart)) {
+                counterOfPairsThatOverlapsAtAll++;
+            }
         }
-        return result;
+        System.out.println("Part 1 = " + counterOfPairsThatOneSectionIsWithinSecondOne);
+        System.out.println("Part 2 = " + counterOfPairsThatOverlapsAtAll);
     }
-
 
     public static void main(String[] args) throws IOException {
         String pathToInputFile = "src/main/resources/day04/input.txt";
         List<String> inputLines = MyUtilities.getInputLines(pathToInputFile);
-
-
-
-        System.out.println("Part 1 = " + getResultForPartOneRules(inputLines));
-        System.out.println("Part 2 = " + getResultForPartTwoRules(inputLines));
+        goOverBigListOfTheSectionAssignmentsForElfs(inputLines);
     }
 }
